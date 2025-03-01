d6585779afd566c39b2197341a15d0bd9f033a32
Mohammad Amanullah <ullahaman072003@gmail.com>
2025-03-02 02:21:55 +0530

    updated errors

--- src/models/User.js
+++ src/models/User.js
@@ -1,35 +1,57 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
+        trim: true
     },
     email: {
         type: String,
         required: true,
         unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
     },
     gender: {
         type: String,
         enum: ['male', 'female', 'other'],
-        required: true,
+        required: true
     },
     age: {
         type: Number,
-        required: true,
-        min: 0,
+        required: true
     },
-    password: {
-        type: String,
-        required: true,
-    },
-    roles: [{
+    role: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Role',
-    }],
+        required: true
+    }
 }, { timestamps: true });
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Method to compare passwords
+userSchema.methods.comparePassword = async function(candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
-export default User;
+module.exports = User;
