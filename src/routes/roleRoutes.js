de46e7f0ea33a68f0ee827608a1dbd39df43e008
Mohammad Amanullah <ullahaman072003@gmail.com>
2025-03-02 02:22:11 +0530

    updated errors

--- src/routes/roleRoutes.js
+++ src/routes/roleRoutes.js
@@ -1,19 +1,20 @@
 const express = require('express');
-const { addRole, editRole, deleteRole, getRoles } = require('../controllers/roleController');
-const { roleCheck } = require('../middleware/roleCheck');
+const { addRole, updateRole, deleteRole, getRoles } = require('../controllers/roleController');
+const roleCheck = require('../middleware/roleCheck');
+const auth = require('../middleware/auth');
 
 const router = express.Router();
 
 // Route to add a new role
-router.post('/', roleCheck('admin'), addRole);
+router.post('/', auth, roleCheck(['admin']), addRole);
 
 // Route to edit an existing role
-router.put('/:id', roleCheck('admin'), editRole);
+router.put('/:id', auth, roleCheck(['admin']), updateRole);
 
 // Route to delete a role
-router.delete('/:id', roleCheck('admin'), deleteRole);
+router.delete('/:id', auth, roleCheck(['admin']), deleteRole);
 
 // Route to get all roles
-router.get('/', getRoles);
+router.get('/', auth, getRoles);
 
 module.exports = router;
