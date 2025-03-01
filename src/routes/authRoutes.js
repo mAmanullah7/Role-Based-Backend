8d65339622601dee12c8e513aa620100735df67c
Mohammad Amanullah <ullahaman072003@gmail.com>
2025-03-02 02:22:00 +0530

    updated errors

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import { signup, login } from '../controllers/authController.js';
+const express = require('express');
+const { register, login } = require('../controllers/authController');
 
 const router = express.Router();
 
-router.post('/signup', signup);
+router.post('/signup', register); // Note: function is named "register" in authController, not "signup"
 router.post('/login', login);
 
-export default router;
+module.exports = router;
